c74eefa8cd657a1939070e5a04ac1f168c1b1b2f
zaha <zaharyuuichi.kensyu@gmail.com>
2023-04-24 15:23:59 +0900

    -javarecord

---
+++ src/record/Person.java
@@ -0,0 +1,52 @@
+package record;
+
+public class Person {
+    record person(String name , String furigana ,int age){};
+
+    //Maxを返すメソッド
+    public static int AgeMax(person[] persons){
+        var max = persons[0].age;
+        for(var i = 0; i < persons.length; i++){
+            if(max < persons[i].age){
+                max = persons[i].age;
+            }
+        }
+        return max;
+    }
+
+    //Minを返すメソッド
+    public static int AgeMin(person[] persons){
+        var min = persons[0].age;
+        for(var i = 0; i < persons.length; i++){
+            if(min > persons[i].age){
+                min = persons[i].age;
+            }
+        }
+        return min;
+    }
+
+    public static int[] BabbleSort(person[] persons){
+        var tmp = 0;
+        int[] ageSort = ArrayToRecord(persons);//配列に変換された値を受け取る
+
+        for(var low = (persons.length - 1); low > 0; low--){
+            for(var upper = 0; upper  < low; upper++){
+                if(ageSort[upper] > ageSort[upper + 1]){
+                    tmp = ageSort[upper+1];
+                    ageSort[upper+1] = ageSort[upper];
+                    ageSort[upper] = tmp;
+                }
+            }
+        }
+        return ageSort;
+    }
+
+    //配列コピー
+    private static int[]ArrayToRecord(person[] persons){
+        int[] array = new int[persons.length];
+        for(var i = 0; i < persons.length; i++){
+            array[i] = persons[i].age;
+        }
+        return array;
+    }
+}
---
+++ src/record/PersonTest.java
@@ -0,0 +1,28 @@
+package record;
+import record.Person;
+
+public class PersonTest {
+    public static void main(String[] args){
+        Person.person[] persons = {
+                new Person.person("月","ゲツ",50),
+                new Person.person("火","カ",1),
+                new Person.person("水","スイ",14),
+                new Person.person("木","モク",2),
+                new Person.person("金","キン",0)
+        };
+
+
+        //最年少と最年長を表示
+        System.out.println("最年小 " + Person.AgeMin(persons) + " 最年長 " + Person.AgeMax(persons));
+
+        //ソートされた配列を表示
+        int[] ageSort = new int[persons.length];
+        ageSort = Person.BabbleSort(persons);
+
+        //出力
+        System.out.print("年齢順");
+        for(var i = 0; i < persons.length; i++) {
+            System.out.print(ageSort[i]+" ");
+        }
+    }
+}
