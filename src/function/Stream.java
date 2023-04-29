7530044e866684c64e778e573e135e8671403ab0
zaha <zaharyuuichi.kensyu@gmail.com>
2023-04-29 15:43:27 +0900

    Stream

--- src/function/Stream.java
+++ src/function/Stream.java
@@ -6,11 +6,11 @@ public class Stream {
     public static void main(String[] args) {
         record Student(String name, int score) {}
 
-        var student1 = new Student("player1", 20);
+        var student1 = new Student("player1", 30);
         var student2 = new Student("player2", 40);
-        var student3 = new Student("player3", 60);
+        var student3 = new Student("player3", 20);
         var student4 = new Student("player4", 80);
-        var student5 = new Student("player5", 100);
+        var student5 = new Student("player5", 10);
 
         List<Student> students = new ArrayList<Student>();
         students.add(student1);
@@ -19,6 +19,9 @@ public class Stream {
         students.add(student4);
         students.add(student5);
 
-        students.size();
+        System.out.println("合計値 "+students.stream().mapToInt(s -> s.score).sum());
+        students.stream().filter(s->s.score >= 80).forEach(System.out::println);
+        students.sort((s1,s2)->s2.score - s1.score);
+        students.stream().forEach(s->System.out.println("名前 "+s.name() + " スコア "+s.score));
     }
 }
